40c72b0e6d9cc0c5c67fa706fa5e707d59e7ecdd
Anna M <83anei@gmail.com>
2021-10-30 01:37:34 +0100

    Change

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask import request, redirect
 from flask import render_template
-main = Flask(__name__)
+app = Flask(__name__)
 
 
-@main.route('/about_me', methods=['GET'], endpoint='action1')
+@app.route('/about_me', methods=['GET'], endpoint='action1')
 def action1():
     return render_template("web1.html")
 
 
-@main.route('/contact', methods=['GET', 'POST'], endpoint='action2')
+@app.route('/contact', methods=['GET', 'POST'], endpoint='action2')
 def action2():
     if request.method == 'GET':
         return render_template("web2.html")
@@ -18,6 +18,6 @@ def action2():
         return redirect("/send")
 
 
-@main.route('/send', methods=['GET'], endpoint='action3')
+@app.route('/send', methods=['GET'], endpoint='action3')
 def action3():
     return render_template("return_message.html")
